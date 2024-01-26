a1f6942b5f7ef1db4ee8b5e62fcd30030da77f41
Frederick <120729769+sharpartzgh@users.noreply.github.com>
2024-01-26 21:02:54 +0000

    text display on mobile fixed

--- src/App.jsx
+++ src/App.jsx
@@ -34,12 +34,12 @@ function App() {
       <div className='  dark:bg-[#202C36] w-full min-h-screen'>
           <header className='flex  font-Nunito text-[#111517] bg-[#FFFFFF] dark:bg-[#2B3844] dark:text-[#FFFFFF] w-full justify-between py-6 px-10 shadow-lg'>
               <div>
-                <h1 className=' text-[24px] font-extrabold mobile:text-[18px]'>Where in the world?</h1>
+                <h1 className=' text-[24px] font-extrabold mobile:text-[16px]'>Where in the world?</h1>
               </div>
-              <div className='flex text-center items-center gap-2'>
+              <div className='flex text-center items-center gap-1'>
               {darkMode ? <IoMoonOutline className=' mobile:h-5 mobile-w-5 cursor-pointer h-6 w-6' onClick={()=> setDarkMode(!darkMode)}/>  :
                 <BsSun onClick={()=> setDarkMode(!darkMode)} className=' cursor-pointer h-6 w-6'/>}
-                <span className=' font-semibold text-[24px] mobile:text-[16px] text '>{darkMode ? 'Light Mode' : 'Dark Mode'}</span>
+                <span className=' font-semibold text-[24px] mobile:text-[14px] text '>{darkMode ? 'Light Mode' : 'Dark Mode'}</span>
               </div>
             </header>
           <div>
